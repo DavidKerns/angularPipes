85953b22a25e99038a1b7f127ad6bed66a29953e
DavidKerns <davidkerns@yahoo.com>
2017-11-04 15:34:56 -0400

    new input

---
+++ src/app/custom-pipes/custom-pipes.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-pipes',
+  templateUrl: './custom-pipes.component.html',
+  styleUrls: ['./custom-pipes.component.css']
+})
+export class CustomPipesComponent {
+
+}
